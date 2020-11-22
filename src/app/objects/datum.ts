5fe62843cb7cff9f7bcd97054fccc2876700abad
simonmarien <simonmarien@hotmail.be>
2020-11-22 20:09:39 +0100

    Fetch data + create datum object

---
+++ src/app/objects/datum.ts
@@ -0,0 +1,54 @@
+export class Datum {
+  private _name: string
+  private _startDate: Date
+  private _endDate: Date = null
+  private _description: string
+
+
+  constructor(name: string, startDate: Date, description: string, endDate?: Date) {
+    this._name = name;
+    this._startDate = startDate;
+    this._endDate = endDate;
+    this._description = description;
+  }
+
+  getDutchMonth(date: Date): string{
+    switch (date.getMonth()){
+      case 0: return 'Januari'
+      case 1: return 'Februari'
+      case 2: return 'Maart'
+      case 3: return 'April'
+      case 4: return 'Mei'
+      case 5: return 'Juni'
+      case 6: return 'Juli'
+      case 7: return 'Augustus'
+      case 8: return 'September'
+      case 9: return 'Oktober'
+      case 10: return 'November'
+      case 11: return 'December'
+      default: return ''
+    }
+  }
+
+  isOneDay(): boolean {
+    if (this._endDate===null)
+      return true
+    else return false
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get startDate(): Date {
+    return this._startDate;
+  }
+
+  get endDate(): Date {
+    return this._endDate;
+  }
+
+  get description(): string {
+    return this._description;
+  }
+}
--- src/app/pages/kalender/kalender.component.ts
+++ src/app/pages/kalender/kalender.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs';
+import {AngularFirestore} from '@angular/fire/firestore';
+import {Datum} from '../../objects/datum';
 
 @Component({
   selector: 'app-kalender',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KalenderComponent implements OnInit {
 
-  constructor() { }
+  dates: Observable<any>
+  refactored_dates: Datum[]
+  loaded: boolean = false
+  constructor(private afs: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.dates = this.afs.collection('dates', ref => ref.orderBy('beginDate')).valueChanges()
+
+    this.dates.subscribe(date => {
+      if (date.endDate.length===0)
+        this.refactored_dates.push(new Datum(date.name,date.beginDate,date.description))
+      else
+        this.refactored_dates.push(new Datum(date.name,date.beginDate,date.description, date.endDate))
+      this.loaded = true
+    })
+    this.refactored_dates.sort((a,b)=> (a.startDate.getDate() > b.startDate.getDate()) ? 1 : -1)
   }
+
 
 }
