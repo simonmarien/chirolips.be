6574498c6df8ded2c3736064112a275e6430ec71
simonmarien <simonmarien@hotmail.be>
2021-05-13 17:44:01 +0200

    Fix deployment url to remove angular directory

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-ng build --prod --base-href "https://www.chirolips.be/angular/" && npx angular-cli-ghpages --dir=dist/lips-angular
+ng build --prod --base-href "https://www.chirolips.be/" && npx angular-cli-ghpages --dir=dist/lips-angular
