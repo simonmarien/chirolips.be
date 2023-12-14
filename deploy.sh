8760cdd7b2b923cde55985239d313a7f7629fbec
simonmarien <simonmarien@hotmail.be>
2023-12-14 20:50:42 +0100

    Add hotlips nov and dec, fix deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-export NODE_OPTIONS=--openssl-legacy-provider && ng build --prod --base-href "https://www.chirolips.be/" && npx angular-cli-ghpages --dir=dist/lips-angular
+ng build --prod --base-href "https://www.chirolips.be/" && npx angular-cli-ghpages --dir=dist/lips-angular
